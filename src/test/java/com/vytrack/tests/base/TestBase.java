df4aaff4deca01aa82bc182ef84523b86f81dd70
yelcan <70501815+yelcan@users.noreply.github.com>
2022-03-05 10:52:52 -0500

    initial Setup

---
+++ src/test/java/com/vytrack/tests/base/TestBase.java
@@ -0,0 +1,16 @@
+package com.vytrack.tests.base;
+
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestBase {
+
+    @BeforeMethod
+    public void setUp(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
+    }
+
+
+}
